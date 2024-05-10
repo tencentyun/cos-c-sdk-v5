e7ede144840142ca194ede83d053fda6efa26a14
huberyxxiao <huberyxxiao@tencent.com>
2024-05-10 14:25:56 +0800

    ut

--- cos_c_sdk_ut/test_all.c
+++ cos_c_sdk_ut/test_all.c
@@ -28,7 +28,7 @@ static const struct testlist {
     {"test_cos_progress", test_cos_progress},
     {"test_cos_resumable", test_cos_resumable},
     {"test_cos_sys", test_cos_sys},
-    {"test_cos_sys", test_cos_crc},
+    {"test_cos_crc", test_cos_crc},
     {"LastTest", NULL}
 };
 
