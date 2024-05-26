f8a845128659a44bd006c7b7a4bfe658fa3d113a
huberyxxiao <huberyxxiao@tencent.com>
2024-05-26 16:38:26 +0800

    ut

--- gen_lcov.sh
+++ gen_lcov.sh
@@ -19,9 +19,10 @@ lcov -d build -z
 lcov -d build -b . -no-external -initial -c -o arvinzhu_init.info
 
 # run
-./build/build/Coverage/bin/cos_c_sdk_ut
+cd build/build/Coverage/
+./bin/cos_c_sdk_ut
 # second
-
+cd ../../..
 lcov -d build -b . -no-external -c -o arvinzhu.info
 
 # filt
