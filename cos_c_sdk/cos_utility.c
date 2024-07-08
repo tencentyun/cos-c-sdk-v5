4ed339e724c7289706944b2bbb81dbc370336002
huberyxxiao <huberyxxiao@tencent.com>
2024-07-08 17:36:08 +0800

    修复object_key检查逻辑

--- cos_c_sdk/cos_utility.c
+++ cos_c_sdk/cos_utility.c
@@ -213,11 +213,20 @@ char ** split(const char * s, char delim, int * returnSize) {
     return ans;
 }
 
-int object_key_simplify_check(const char * path){
+int object_key_simplify_check(const char * object_path){
     if (!get_object_key_simplify_check()){
         return 1;
     }
     int namesSize = 0;
+    char *path;
+    if (object_path[0] == '/'){
+        path = (char *)malloc(sizeof(char *) * (strlen(object_path) + 1));
+        strcpy(path, object_path);
+    }else {
+        path = (char *)malloc(sizeof(char *) * (strlen(object_path) + 2));
+        path[0] = '/';
+        strcpy(path + 1, object_path);
+    }
     int n = strlen(path);
     char ** names = split(path, '/', &namesSize);
     char ** stack = (char **)malloc(sizeof(char *) * namesSize);
@@ -253,6 +262,7 @@ int object_key_simplify_check(const char * path){
     }
     free(names);
     free(stack);
+    free(path);
     if (strlen(ans) == 1 && ans[0] == '/'){
         free(ans);
         return 0;
